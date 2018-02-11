a94b6f3174dbf6aa68de4796939dce3dd77a7ea1
Norbert Bagi <norb@Norberts-MacBook-Pro.local>
2018-02-11 23:02:31 +0100

    new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { BookComponent } from './components/book/book.component';
+import { CartComponent } from './components/cart/cart.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    BookComponent,
+    CartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
